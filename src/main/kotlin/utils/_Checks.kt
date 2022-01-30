e71db085c4f4651785326e1bb2509e9930e5627c
Ayfri <pierre.ayfri@gmail.com>
2022-01-30 15:47:36 +0100

    fix: Fix translations accents.

--- src/main/kotlin/utils/_Checks.kt
+++ src/main/kotlin/utils/_Checks.kt
@@ -1,11 +1,11 @@
 package utils
 
-import com.kotlindiscord.kord.extensions.checks.memberFor
 import com.kotlindiscord.kord.extensions.checks.types.CheckContext
+import com.kotlindiscord.kord.extensions.checks.userFor
 import dev.kord.core.event.Event
 import ownerID
 
 suspend fun <T : Event> CheckContext<T>.isOwner() {
 	if (!passed) return
-	if (ownerID != memberFor(event)!!.id) fail()
+	if (ownerID != userFor(event)!!.id) fail()
 }
